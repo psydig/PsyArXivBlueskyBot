385f9293fc149e6350fd4a0217bc8efa404aabe7
Remo Schmutz <remoschmutz@bluewin.ch>
2023-10-20 15:46:43 +0200

    automated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
+import fs from 'fs';
 
-const text = await Bot.run(getPostText, { dryRun: true });
+interface Paper {
+  title: string;
+  link: string;
+}
 
-console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+const POSTED_PAPERS_PATH = './postedPapers.json';
+const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
+
+async function main() {
+  const textData = await getPostText();
+  
+  if (textData) {
+    const { title, link, formattedText } = textData;
+    
+    if (!postedPapers.papers.some((paper: Paper) => paper.title === title && paper.link === link)) {
+      await Bot.run(() => Promise.resolve(formattedText));
+      
+      // Update the postedPapers.json
+      postedPapers.papers.push({ title, link });
+      fs.writeFileSync(POSTED_PAPERS_PATH, JSON.stringify(postedPapers, null, 2));
+
+      console.log(`[${new Date().toISOString()}] Posted: "${formattedText}"`);
+    } else {
+      console.log(`[${new Date().toISOString()}] Already posted: "${title}"`);
+    }
+  } else {
+    console.log(`[${new Date().toISOString()}] No new posts to publish.`);
+  }
+}
+
+main();
+
