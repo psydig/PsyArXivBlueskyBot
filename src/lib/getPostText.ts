d3990828442b432551ff3c5f34ef323348630fb3
Remo Schmutz <remoschmutz@bluewin.ch>
2023-10-21 12:13:09 +0200

    update

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,29 +1,39 @@
-import fs from 'fs';
 import Parser from 'rss-parser';
+import fs from 'fs';
 
-const FEED_URL = 'https://osfpreprints-feed.herokuapp.com/PsyArXiv.rss';
+interface Paper {
+  title: string;
+  link: string;
+}
+
+const FEED_URL = 'https://osfpreprints-feed.herokuapp.com/PsyArXiv.rss';  
 const POSTED_PAPERS_PATH = './postedPapers.json';
+const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
+
 const ONE_DAY = 24 * 60 * 60 * 1000;  // One day in milliseconds
 
 export default async function getPostText() {
   const parser = new Parser();
   const feed = await parser.parseURL(FEED_URL);
-  const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
-
-  const currentDate = new Date();
 
   for (const item of feed.items) {
     const publicationDate = item.pubDate ? new Date(item.pubDate) : new Date();
+    const currentDate = new Date();
 
-    // Check if the paper is already posted by comparing title and link
-    const isAlreadyPosted = postedPapers.papers.some(paper => paper.title === item.title && paper.link === item.link);
+    // Check if the paper has been posted before
+    const isAlreadyPosted = postedPapers.papers.some((paper: Paper) => paper.title === item.title && paper.link === item.link);
 
-    // If publication is newer than one day, and it's not already posted
+    // If publication is newer than one week and hasn't been posted before, return it for posting
     if (!isAlreadyPosted && (currentDate.getTime() - publicationDate.getTime() <= ONE_DAY)) {
-      return `${item.title}: ${item.link}`;  // Format this as you'd like
+      return {
+        title: item.title,
+        link: item.link,
+        formattedText: `${item.title}: ${item.link}` // Format this as you'd like
+      };
     }
   }
 
-  // If no new papers are found
+  // If no suitable paper is found within a days's range
   return null;
 }
+
