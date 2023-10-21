ab6e0b1bea942febe15b5071ea4816dbd6d9169d
Remo Schmutz <remoschmutz@bluewin.ch>
2023-10-21 15:33:43 +0200

    big push

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-// index.ts
-
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 import fs from 'fs';
-import { execSync } from 'child_process';  // <-- Add this import
+import { execSync } from 'child_process';
 
 interface Paper {
   title: string;
@@ -14,31 +12,30 @@ const POSTED_PAPERS_PATH = './postedPapers.json';
 const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
 
 async function main() {
-  const textData = await getPostText();
-  
-  if (textData) {
-    const { title, link, formattedText } = textData;
-    
-    if (!postedPapers.papers.some((paper: Paper) => paper.title === title && paper.link === link)) {
-      await Bot.run(() => Promise.resolve(formattedText));
-      
-      // Update the postedPapers.json
-      postedPapers.papers.push({ title, link });
-      fs.writeFileSync(POSTED_PAPERS_PATH, JSON.stringify(postedPapers, null, 2));
+  const papersData = await getPostText();
 
-      console.log(`[${new Date().toISOString()}] Posted: "${formattedText}"`);
+  if (papersData && papersData.length > 0) {
+    for (const textData of papersData) {
+      const { title, link, formattedText } = textData;
 
-      // Commit and push the changes
-      execSync('git add ' + POSTED_PAPERS_PATH);
-      execSync('git commit -m "Added new posted paper"');
-      execSync('git push origin main'); 
+      if (!postedPapers.papers.some((paper: Paper) => paper.title === title && paper.link === link)) {
+        await Bot.run(() => Promise.resolve(formattedText));
+        
+        postedPapers.papers.push({ title, link });
+        fs.writeFileSync(POSTED_PAPERS_PATH, JSON.stringify(postedPapers, null, 2));
 
-    } else {
-      console.log(`[${new Date().toISOString()}] Already posted: "${title}"`);
+        console.log(`[${new Date().toISOString()}] Posted: "${formattedText}"`);
+      } else {
+        console.log(`[${new Date().toISOString()}] Already posted: "${title}"`);
+      }
     }
+    execSync('git add ' + POSTED_PAPERS_PATH);
+    execSync('git commit -m "Added new posted papers"');
+    execSync('git push origin main'); 
   } else {
     console.log(`[${new Date().toISOString()}] No new posts to publish.`);
   }
 }
 
 main();
+
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -6,7 +6,7 @@ interface Paper {
   link: string;
 }
 
-const FEED_URL = 'https://osfpreprints-feed.herokuapp.com/PsyArXiv.rss';  
+const FEED_URL = 'https://osfpreprints-feed.herokuapp.com/PsyArXiv.rss';
 const POSTED_PAPERS_PATH = './postedPapers.json';
 const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
 
@@ -15,25 +15,23 @@ const ONE_DAY = 24 * 60 * 60 * 1000;  // One day in milliseconds
 export default async function getPostText() {
   const parser = new Parser();
   const feed = await parser.parseURL(FEED_URL);
+  const papersToPost = [];
 
   for (const item of feed.items) {
     const publicationDate = item.pubDate ? new Date(item.pubDate) : new Date();
     const currentDate = new Date();
 
-    // Check if the paper has been posted before
     const isAlreadyPosted = postedPapers.papers.some((paper: Paper) => paper.title === item.title && paper.link === item.link);
 
-    // If publication is newer than one week and hasn't been posted before, return it for posting
     if (!isAlreadyPosted && (currentDate.getTime() - publicationDate.getTime() <= ONE_DAY)) {
-      return {
+      papersToPost.push({
         title: item.title,
         link: item.link,
-        formattedText: `${item.title}: ${item.link}` // Format this as you'd like
-      };
+        formattedText: `${item.title}: ${item.link}`
+      });
     }
   }
 
-  // If no suitable paper is found within a days's range
-  return null;
+  return papersToPost.length > 0 ? papersToPost : null;
 }
 
